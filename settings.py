5a73265bb103199816cd89f047e993278b9e4116
oneku16 <eku.ulanov@yandex.com>
2024-03-20 01:53:36 +0600

    done tech part

--- settings.py
+++ settings.py
@@ -3,8 +3,15 @@ from os import (
     environ,
 )
 
-
 DIR_PATH: str = path.dirname(path.realpath(__file__))
 
-SKIP_SHEETS: set = {'задача'}
+POSTGRES_USER: str = environ.get('POSTGRES_USER', default='developer')
+POSTGRES_PASSWORD: str = environ.get('POSTGRES_PASSWORD', default='developer')
+POSTGRES_HOST: str = environ.get('POSTGRES_HOST', default='localhost')
+POSTGRES_PORT: str = environ.get('POSTGRES_PORT', default='5432')
+POSTGRES_DB: str = environ.get('POSTGRES_DB', default='developer')
 
+DATA_BASE_URL: str = f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}'
+
+
+SKIP_SHEETS: set = {'задача'}
